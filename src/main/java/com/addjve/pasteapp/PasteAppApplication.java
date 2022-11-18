9f36626c9d3f1a6b490ecd1b1b06d39a4ed99a5d
addJve <ksldoofur8533@proton.me>
2022-11-18 17:04:05 +0200

    init

---
+++ src/main/java/com/addjve/pasteapp/PasteAppApplication.java
@@ -0,0 +1,13 @@
+package com.addjve.pasteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasteAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/addjve/pasteapp/PasteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addjve.pasteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
